14f72f7da52f87d6019f2b28dff75e0b8b62df49
siddarthnachannagari <mnachan@purdue.edu>
2025-07-16 22:50:00 -0400

    Fixed up the synthetic data to be proper, taking into account certain baises for the location, the payees, the frequency of types of transactions, etc... Really like the synthetic data that was created, super clean, super useful, understandable, and takes into account multiple factors.

--- data_to_aws_cloud.py
+++ data_to_aws_cloud.py
@@ -3,9 +3,9 @@ import boto3
 s3 = boto3.client('s3')
 
 # File you want to upload
-file_name = 'synthetic_transactions_labeled.csv'
+file_name = 'synthetic_transactions_1M.csv'
 bucket_name = 'accidentaltransferproject'
-object_name = 'data/synthetic_transactions_labeled.csv'  
+object_name = 'data/synthetic_transactions_1M.csv'  
 
 s3.upload_file(file_name, bucket_name, object_name)
 
